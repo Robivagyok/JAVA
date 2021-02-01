e4ad6c9f3d8d54981efa56ad187cb69c00f0fa40
Robivagyok <71004388+Robivagyok@users.noreply.github.com>
2021-02-01 11:15:43 +0100

    Add files via upload

--- TombKiiras.java
+++ TombKiiras.java
@@ -12,20 +12,38 @@ import java.util.Scanner;
  * @author Robi
  */
 public class TombKiiras {
+    public static void rendez(int[] tomb){
+    for (int i = 0; i < 10-1; i++) {
+            for (int j = 0; j < 10-i-1; j++) {
+                if (tomb[j]>tomb[j+1]){
+                    int temp=tomb[j];
+                    tomb[j]=tomb[j+1];
+                    tomb[j+1]=temp;
+                }
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.print(tomb[i]+" ");
+        }
+}
+    
 public static void kiir(int[] tomb){
     for (int i = 0; i <tomb.length; i++) {
         System.out.print(String.format("%,3d",tomb[i])+" ");
     }
     System.out.println();
 }
+        
 public static void tomb(int[] tomb){
     for (int i = 0; i <10; i++) {
             tomb[i] = (int) (Math.random()*200)-100;
+            
         }
 }
-    /**
-     * @param args the command line arguments
-     */
+public static void elvalaszto(){
+System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");    
+        }
+    
     public static void main(String[] args) {
         Scanner be=new Scanner(System.in);
         System.out.println("Tömbkiírás metódussal");
@@ -38,12 +56,24 @@ public static void tomb(int[] tomb){
         tomb(tomb2);
         tomb(tomb3);
         
-        System.out.println("Az első tömb elemei:");
+        System.out.println("\nAz első tömb elemei:");
         kiir(tomb1);
-        System.out.println("A második tömb elemei:");
+        
+        System.out.println("\nA második tömb elemei:");
         kiir(tomb2);
-        System.out.println("A harmadik tömb elemei:");
+        
+        System.out.println("\nA harmadik tömb elemei:");
         kiir(tomb3);
+        
+        System.out.println("\nAz első tömb rendezve:");
+        rendez(tomb1);
+        
+        System.out.println("\nA második tömb rendezve:");
+        rendez(tomb2);
+        
+        System.out.println("\nA harmadik tömb rendezve:");
+        rendez(tomb3);
+        
     }
     
 }
